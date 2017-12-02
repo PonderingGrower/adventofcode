81bbf3a3ada7be39d0017c64d0608290d718bcf0
Jakub Sokołowski <panswiata@gmail.com>
2017-12-02 15:49:43 +0000

    add solution to day 2

---
+++ 02/answer.js
@@ -0,0 +1,21 @@
+const assert = require('assert')
+const fs = require('fs')
+
+const calc = (data) => {
+  return data.reduce((sum, row) => 
+    sum + Math.abs(Math.max(...row) - Math.min(...row))
+  , 0)
+}
+
+data = [
+  [5, 1, 9, 5],
+  [7, 5, 3],
+  [2, 4, 6, 8],
+]
+
+assert.equal(calc(data), 18)
+
+data = fs.readFileSync('input').slice(0, -1).toString('utf8')
+data = data.split('\n').map(row => row.split('\t'))
+
+console.log(calc(data))
